b6225bbbc4f2aafb2c8d181ef087b4a722046b71
我就是我 <138080354+wjsw3369@users.noreply.github.com>
2023-08-25 21:24:03 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -57,7 +57,7 @@ Install_Node() {
 
   rm -rf  node-"$node"-linux-"$arch".tar.gz
 
-  wget https://vip.123pan.cn/1815631588/Minecraft/%E6%9C%8D%E5%8A%A1%E7%AB%AF%E9%9D%A2%E6%9D%BF/node-v14.19.1-linux-x64.tar.gz
+  wget https://nodejs.org/dist/"$node"/node-"$node"-linux-"$arch".tar.gz
 
   tar -zxf node-"$node"-linux-"$arch".tar.gz
 
